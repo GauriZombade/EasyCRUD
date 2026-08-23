a1a40c920cca45584605fec5b621a3b0dea19c7f
Gauri Zombade <zombadegauri02@gmail.com>
2026-08-24 03:21:42 +0530

    jenkinsfile-argocd & backend ,frontend update

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ data "aws_vpc" "default" { // Fetch default VPC
 
 variable "cluster_name" { // Variable for cluster name
   type    = string
-  default = "my-eks-cluster1"
+  default = "cluster-2"
 }
 
 data "aws_subnets" "default" { // Fetch  all subnets in the default VPC
